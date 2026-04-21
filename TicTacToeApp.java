ca45e0f2d023bafb3ebe89a5965c2495f86f8e40
ruchitavishnoi <vishnoiruchita@gmail.com>
2026-04-21 15:28:45 +0530

    Convert user-entered slot number into row and column indices.

---
+++ TicTacToeApp.java
@@ -0,0 +1,17 @@
+public class TicTacToeApp {
+
+    public static void main(String[] args) {
+        int slot = 7;
+        System.out.println("Slot: " + slot);
+        System.out.println("Row: " + getRowFromSlot(slot));
+        System.out.println("Column: " + getColFromSlot(slot));
+    }
+
+    static int getRowFromSlot(int slot) {
+         return (slot - 1) / 3;
+    }
+
+    static int getColFromSlot(int slot) {
+         return (slot - 1) % 3;
+    }
+}
